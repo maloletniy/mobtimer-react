51c2eb3cf9b48595b37fd807d9dae435a12fb3a3
Igor Maloletnii <igor.maloletnii@kuehne-nagel.com>
2017-12-04 18:19:23 +0200

    added ui kit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class App extends React.Component {
-  render() {
-    return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>);
-  }
+  render() {return (
+    <MuiThemeProvider>
+      <AppBar title='My AppBar' />
+      <RaisedButton label='Default' />
+    </MuiThemeProvider> 
+    )}
+
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-//     template: './client/index.html',
-//     filename: 'index.html',
-//     inject: 'body'
-// })
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './client/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
 
 const APP_DIR = path.resolve(__dirname, 'client');
 const BUILD_DIR = path.resolve(__dirname, 'client/build');
@@ -16,14 +16,13 @@ module.exports = {
     },
     devServer: {
         port: 3000,
-        contentBase: './client'
+        contentBase: './client/build'
     },
     module: {
         loaders: [
             { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
             { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
         ]
-    }
+    },
+    plugins: [HtmlWebpackPluginConfig]
 }
-
-// plugins: [HtmlWebpackPluginConfig]
